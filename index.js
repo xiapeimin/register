9094fb12cc2bba3584b1f9d44a7a2128e1ebe1a2
xiapeimin <2536733090@qq.com>
2020-03-17 16:01:19 +0800

    update

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ $(function(){
     var $pmsg = $('#phone-validation-msg');
     var $smsg = $('#pwd-validation-msg');
     var $passmsg = $('#pass-validation-msg');
+    var $pass = $('#pass');
+    var $check = $('#check');
+    var $regok = $('.regok');
+    var f1=0,f2=0,f3=0;
 
 
     //用户名数据校验
@@ -17,9 +21,11 @@ $(function(){
         if((user_reg.test(username) && !num_reg.test(username)) || username===''){
             $user.css('border','1px solid #e2dada');
             $umsg.html('');
+            f1=1;
         }else{
             $user.css('border','1px solid red');
             $umsg.html('用户名仅支持中英文，数字和下划线且不能为纯数字');
+            f1=0;
         }
         
     });
@@ -30,22 +36,26 @@ $(function(){
         if(tel_reg.test(phone) || phone===''){
             $phone.css('border','1px solid #e2dada');
             $pmsg.html('');
+            f2=1;
         }else{
             $phone.css('border','1px solid red');
             $pmsg.html('手机号码格式不正确');
+            f2=0;
         }       
     });
     //密码校验：字母、数字、符号至少包含两种，不允许有空格，中文；8-14位
     $pwd.blur(function () { 
         var pwd = $pwd.val();
         var pwd_reg = /(?!^\d+$)(?!^[A-Za-z]+$)(?!^[^A-Za-z0-9]+$)(?!^.*[\u4E00-\u9FA5].*$)^\S{8,14}$/;
-        console.log('ddddddddd');
+        
         if(pwd_reg.test(pwd) || pwd===''){
             $pwd.css('border','1px solid #e2dada');
             $smsg.html('');
+            f3=1;
         }else{
             $pwd.css('border','1px solid red');
             $smsg.html('密码设置不符合要求');
+            f3=0;
         }
         
     });
@@ -76,6 +86,22 @@ $(function(){
 
     });
 
-    
+    //所有数据非空且合法即可注册
+    var flag = 0;
+    $check.click(function(){
+        if(flag===0){
+            flag=1;
+            if(flag===1 && f1===1 && f2===1 && f3===1){
+                if($user.val()!=='' && $phone.val()!=='' && $pwd.val()!=='' && $pass.val()!==''){
+                    $regok.css('opacity','1');
+                    $regok.removeAttr('disabled');
+                }               
+            }
+        }else{
+            flag=0;
+        }
+    });
+
+
 
 });
